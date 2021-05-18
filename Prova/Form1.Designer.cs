d02f97491321b0345cccbb104109659140c632b8
Pedroteodoro25 <78798990+Pedroteodoro25@users.noreply.github.com>
2021-05-18 12:16:39 -0300

    Add files via upload

---
+++ Prova/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace P0030482021028
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.listVendas = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Location = new System.Drawing.Point(268, 139);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(178, 111);
+            this.btnVerificar.TabIndex = 0;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // listVendas
+            // 
+            this.listVendas.FormattingEnabled = true;
+            this.listVendas.Location = new System.Drawing.Point(542, 97);
+            this.listVendas.Name = "listVendas";
+            this.listVendas.Size = new System.Drawing.Size(246, 264);
+            this.listVendas.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listVendas);
+            this.Controls.Add(this.btnVerificar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.ListBox listVendas;
+    }
+}
+
---
+++ Prova/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+using System.Windows.Forms;
+
+namespace P0030482021028
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            
+            double[,] vendas = new double[8, 4];
+            double totalSemanas = 0, totalMeses = 0;
+            int mes, semana;
+            string ar;
+
+            
+            for (mes = 0; mes < vendas.GetLength(0); mes++)
+            {
+                
+                for (semana = 0; semana < vendas.GetLength(1); semana++)
+                {
+                    
+                    ar = Interaction.InputBox($"Semana #{semana + 1}", $"Mês #{mes + 1}");
+
+                   
+                    if (ar == string.Empty)
+                        return;
+
+                    
+                    if (!double.TryParse(ar, out vendas[mes, semana]))
+                    {
+                        MessageBox.Show("Valor informado inválido!\nPor favor, apenas dados númericos.", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        semana--;
+                    }
+                }
+            }
+
+            
+            if (listVendas.Items.Count > 0) 
+            {
+                
+                listVendas.Items.Add("----------------------------------------------------");
+            }
+
+            for (mes = 0; mes < vendas.GetLength(0); mes++)
+            {
+                totalSemanas = 0;
+
+                for (semana = 0; semana < vendas.GetLength(1); semana++)
+                {
+                    listVendas.Items.Add($"Total do mês: {mes + 1} Semana: {semana + 1}: {vendas[mes, semana]:C2}");
+                    totalSemanas += vendas[mes, semana];
+                }
+
+                listVendas.Items.Add($" Total Mês: {totalSemanas:C2}");
+                totalMeses += totalSemanas;
+                listVendas.Items.Add("-------------");
+            }
+
+            listVendas.Items.Add($" Total Geral: {totalMeses:C2}");
+        }
+
+        
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            listVendas.Items.Clear();
+        }
+    }
+}
